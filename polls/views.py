4dde328a8cd8180bda5682c7838c470ca045d640
Sean Kim <sean.hamilton.kim@gmail.com>
2024-10-12 22:01:10 -0700

    step 3

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Polls index")
+    return HttpResponse("""
+    <html>
+        <body>
+            <h2>Hello!</h2>
+            <p>Very cool</p>
+        </body>
+    </html>
+    """)
 
+def detail(request, question_id):
+    return HttpResponse("Question #{}".format(question_id))
+
+def results(request, question_id):
+    response = "Result of question #%s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse(f"Voting on #{question_id}")
